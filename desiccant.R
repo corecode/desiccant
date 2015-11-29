2d0db4e29a5707dc65f457858d200c8e036cba57
Simon Schubert <2@0x2c.org>
2015-11-29 22:42:50 +0100

    works

--- desiccant.R
+++ desiccant.R
@@ -1,8 +1,11 @@
+library(deSolve)
+library(reshape2)
+
 calcMs <- function(W, T, parms) {
     with (as.list(parms), {
         Psat <- ifelse(T > 100,
                        10**(8.14019-1810.94/(244.485+T)),
-                       10**(8.07131-1730.63/(233.426+T)))
+                       10**(8.07131-1730.63/(233.426+T)))/51.71493
         Ms <- 0.622*RH(W) * Psat / (P - 0.378*RH(W) * Psat)
         return(Ms)
     })
@@ -17,21 +20,18 @@ desiccant <- function(time, state, parms, N) {
         ## Re: Reynold's number
         ## r: bed radius
         ## P: inlet pressure
-        ## N: number of Z steps
         ## L: length (N * dz)
         ## Me_in: inlet air water mass fraction
         ## Te_in: inlet air temperature
         ## Hads(W): heat of adsorption
         ## RH(W): isotherm relative humidity
 
-        print(time)
-
+        N <- length(state)/2
         Wavg <- state[1:N]
         Ts <- state[N+1:N]
-        Te <- state[2*N+1:N]
-        Me <- state[3*N+1:N]
 
-        dz <- L/N
+        #print(list(Wavg, Ts))
+
         A <- r**2 * pi                  # area
         p <- 2*r * pi                   # perimeter
 
@@ -39,22 +39,34 @@ desiccant <- function(time, state, parms, N) {
         mdotG <- A * Ga
         KGeff <- 0.704*Ga*Re**-0.51
         Ms <- calcMs(Wavg, Ts, parms)
+        names(Ms) <- NULL
         cpl <- 1884                     # J/kg/K
-        ce <- cpl * Me + 1004 * (1-Me)
         cb <- 4186 * Wavg + 921
-        hc <- 0.683*Ga*Re**-0.51 * ce
+
+        ce <- function(Me) cpl * Me + 1004 * (1-Me)
+        hc <- function(Me) 0.683*Ga*Re**-0.51 * ce(Me)
+
+        dzSolve <- function(t, state, parms) {
+            Mez <- state[1]
+            Tez <- state[2]
+            Msz <- Ms[1+t*(N-1)/L]
+            Tsz <- Ts[1+t*(N-1)/L]
+
+            dMe_dz <- KGeff*p/mdotG * (Msz-Mez) * (1-Mez)
+            dTe_dz <- -p/ce(Mez)/mdotG * (hc(Mez) + cpl*KGeff * (Msz-Mez)) * (Tez-Tsz)
+
+            #print(c(as.integer(t*(N-1)/L), Mez, Tez, dMe_dz, dTe_dz))
+
+            return(list(c(Me=dMe_dz, Te=dTe_dz)))
+        }
+        z_state <- ode(c(Me_in, Te_in), seq(0, L, length.out=N), dzSolve, parms=parms, method=c("rk4"))
+        Me <- z_state[,2]
+        Te <- z_state[,3]
 
         dWavg <- -KGeff*p/A/rhob * (Ms-Me)
-        dTs <- p/A/rhob/cb * (hc * (Te-Ts) - Hads(Wavg)*KGeff * (Ms-Me))
+        dTs <- p/A/rhob/cb * (hc(Me) * (Te-Ts) - Hads(Wavg)*KGeff * (Ms-Me))
 
-        dMe_dz <- KGeff*p/mdotG * (Ms-Me) * (1-Me)
-        dMe_diff <- diff(c(Me_in, Me))
-        dMe <- dMe_dz*dz - dMe_diff
-        dTe_dz <- -p/ce/mdotG * (hc+cpl*KGeff * (Ms-Me)) * (Te-Ts)
-        dTe_diff <- diff(c(Te_in, Te))
-        dTe <- dTe_dz*dz - dTe_diff
-
-        return(list(c(Wavg=dWavg, Ts=dTs, Te=dTe, Me=dMe)))
+        return(list(c(dWavg, dTs), c(Ms=Ms, Me=Me, Te=Te)))
     })
 }
 
@@ -67,18 +79,20 @@ params <- list(
     r=0.0651,
     P=15,
     L=77.5e-3,
-    Me_in=0.0097,
+    Me_in=0.08,
     Te_in=23.7,
+    Wavg_0=0.00,
+    T_0=23.7,
     Hads=function(W) ifelse(W<=0.15, -300*W+2095, 2050),
     RH=function(W) ifelse(W<=0.07, 1.235*W + 267.99*W**2 - 3170.7*W**3 + 10087.16*W**4, 0.3316 + 3.18*W)
 )
 
-initial <- list(
-    Wavg=0.0088,
-    T=23.7
-)
+params$M_0<- calcMs(params$Wavg_0, params$T_0, params)
 
-initial$M <- calcMs(initial$Wavg, initial$T, params)
+solveDesiccant <- function(times, N, params) {
+    state <- c(Wavg=rep(params$Wavg_0, N), Ts=rep(params$T_0, N))
 
-N <- 100
-state <- c(rep(initial$Wavg, N), rep(initial$T, N), rep(initial$T, N), rep(initial$M, N))
+    res <- ode.1D(state, times, desiccant, params, nspec=2, method=c("euler"))
+    res.df <- as.data.frame(res[,c(1,1+N*1:5)])
+    return(melt(res.df, "time"))
+}
